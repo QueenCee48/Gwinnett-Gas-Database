279c67ea858557908f539b0a2b0bce537de4e6c3
Ciera <177058032+QueenCee48@users.noreply.github.com>
2025-07-03 18:38:53 -0400

    Update connectDatabase.php

--- connectDatabase.php
+++ connectDatabase.php
@@ -1,12 +1,8 @@
 <?php
-	$dbname = "cbaucham_gasdb";
-	$dbuser = "cbaucham_guest";
-	$dbpass = "ggcITEC3860@";
+	$dbname ="";
+	$dbuser = "";
+	$dbpass = "";
 	$dbhost = "localhost";
-	
-	// $connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
-	
-	// mysql_select_db($dbname) or die("Could not open the db '$dbname'");
 
 	$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
